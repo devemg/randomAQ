ecfcd7a32589c61c51f22e3da85cd422bcb65053
devemg <garciam.emm@gmail.com>
2021-05-20 23:49:48 -0600

    add home and routing

--- admin-app/src/app/admin/admin-routing.module.ts
+++ admin-app/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListCategoriesComponent } from './components/categories/list-categories/list-categories.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent,
+    children: [
+      {
+        path:'categories',
+        component:ListCategoriesComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- admin-app/src/app/admin/admin.module.ts
+++ admin-app/src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { ListCategoriesComponent } from './components/categories/list-categories/list-categories.component';
 import { NewUpdateCategoryComponent } from './components/categories/new-update-category/new-update-category.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     ListCategoriesComponent,
-    NewUpdateCategoryComponent
+    NewUpdateCategoryComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
