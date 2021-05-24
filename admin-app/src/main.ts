3eb7d00b0c214c45d6b2aab2c7cf3b3d519e2831
devemg <garciam.emm@gmail.com>
2021-05-23 22:43:30 -0600

    install amplify

--- admin-app/src/main.ts
+++ admin-app/src/main.ts
@@ -3,7 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import Amplify, { Auth } from 'aws-amplify';
+/*
+Amplify.configure({
+  Auth:{
 
+  }
+});
+*/
 if (environment.production) {
   enableProdMode();
 }
