21238f260ca57097e4b7b15ec2eaac8386d6cb91
devemg <garciam.emm@gmail.com>
2021-05-24 01:32:04 -0600

    register form and add simplify to auth

--- admin-app/src/app/components/register/register.component.ts
+++ admin-app/src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { PasswordValidator } from './password-validator';
 
 @Component({
   selector: 'app-register',
@@ -9,21 +11,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   registerForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+
+  get registerFControls() { return this.registerForm.controls; }
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
     this.registerForm = this.formBuilder.group({
-      username: ['',Validators.required],
+      username: ['',[Validators.required,Validators.minLength(4)]],
       email: ['',[Validators.required,Validators.email]],
-      password: ['',Validators.required],
+      password: ['',[Validators.required,Validators.minLength(8),Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z]).*$')]],
       confirmPassword: ['',Validators.required]
-    });
+    },{validator: PasswordValidator('password','confirmPassword') });
    }
 
   ngOnInit(): void {
   }
 
   register() {
-    console.log(this.registerForm.value);
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value);
+    }else {
+      console.log(this.registerFControls.errors)
+    }
     
+  }
+
+  /**
+   * Check control forms to return if should show error message in form
+   * @param control 
+   * @param error 
+   * @returns 
+   */
+  isError(control: string, error: string){
+    return this.registerFControls[control].touched 
+    && this.registerFControls[control].hasError(error)
   }
 
 }
--- admin-app/src/app/services/auth.service.ts
+++ admin-app/src/app/services/auth.service.ts
@@ -1,18 +1,78 @@
 import { Injectable } from '@angular/core';
 import { LocalStorageService } from './local-storage.service';
+import { Auth } from '@aws-amplify/Auth';
+import { NewUser } from '../models/new-user';
+import { LogInUser } from '../models/login-user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private localService: LocalStorageService) { }
-
-  login(user: string, password: string) {
-    this.localService.setAuthToken('123');
+  constructor(private localService: LocalStorageService) { 
+    Auth.configure({
+      userPoolId:'us-east-1_hPVsDYBCG',
+			userPoolWebClientId:'44bignuc1ghp4cp8sn3v5alf6k',
+			region: 'us-east-1',
+			//oauth,
+			//identityPoolId: '',
+			//mandatorySignIn,
+			//refreshHandlers,
+			//identityPoolRegion,
+			//clientMetadata,
+    });
   }
 
-  logout() {
+  /**
+   * Register a new user in cognito
+   * @param user 
+   */
+  async register(user: NewUser) {
+    try {
+      const newUser  = await Auth.signUp({
+          username: user.username,
+          password: user.password,
+          attributes: {
+            email: user.email
+          }
+      });
+      console.log(newUser);
+    } catch (error) {
+        console.log('error signing up:', error);
+    }
+  }
+
+  changePassword() {
+
+  }
+
+  completePassword() {
+
+  }
+
+  /**
+   * Login cognito user
+   * @param user 
+   * @param password 
+   */
+  async login(user: LogInUser) {
+    try {
+      const currentUser = await Auth.signIn(user.username, user.password);
+      console.log(currentUser)
+    } catch (error) {
+        console.log('error signing in', error);
+    }
+  }
+
+  /**
+   * logout cognito user
+   */
+  async logout() {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+        console.log('error signing out: ', error);
+    }
     this.localService.removeAuthToken();
   }
 
